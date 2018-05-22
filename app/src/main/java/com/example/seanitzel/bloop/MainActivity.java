7342cd7194b92a989e522984719bf86921893c0f
Seanitzel <zazi.dx@gmail.com>
2018-05-22 07:16:20 +0300

    Set up everything to be efficient!

--- app/src/main/java/com/example/seanitzel/bloop/MainActivity.java
+++ app/src/main/java/com/example/seanitzel/bloop/MainActivity.java
@@ -20,32 +20,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final MediaPlayer[] mp = {MediaPlayer.create(this,R.raw.harmony), MediaPlayer.create(this,R.raw.bunny), MediaPlayer.create(this,R.raw.lying) };
+        final int[] BUTTON_IDS = {
+                R.id.btn1,
+                R.id.btn2,
+                R.id.btn3,
+                R.id.btn4,
+                R.id.btn5,
+                R.id.btn6,
+                R.id.btn7,
+                R.id.btn8,
+                R.id.btn9,
+                R.id.btn10,
+                R.id.btn11,
+                R.id.btn12,
+                R.id.btn13,
+                R.id.btn14,
+                R.id.btn15,
+        };
 
-        btns = new Button[3];
-        btns[0] = (Button) findViewById(R.id.btn1);
-        btns[1] = (Button) findViewById(R.id.btn2);
-        btns[2] = (Button) findViewById(R.id.btn3);
-        btns[0].setBackgroundColor(0xFFFF0000);
-        btns[0].setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view)
-        {
-            mp[0].start();
+        final String[] BUTTON_LABELS = {
+                "Hi",
+                "Yes",
+                "No",
+                "What",
+                "Stop",
+                "Dont",
+                "Sure",
+                "WTF",
+                "Shit",
+                "Life",
+                "Death",
+                "One",
+                "Die",
+                "Love",
+                "Now",
+        };
+        btns = new Button[15];
+        for(int i=0;i<15;i++) {
+            btns[i] = (Button) findViewById(BUTTON_IDS[i]);
+            btns[i].setBackgroundColor(0xFFFF0000);
+            btns[i].setText(BUTTON_LABELS[i]);
+            btns[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mp[0].start();
+                }
+            });
         }
-    });
-        btns[1].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                mp[1].start();
-            }
-        });
-        btns[2].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                mp[2].start();
-            }
-        });
     }
 }
